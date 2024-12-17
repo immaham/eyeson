8fef38d6e24fe859abd4d0169a8aaa3011235a7f
maham <mahaampakzad@gmail.com>
2024-12-17 12:01:32 +0330

    services offer edited

--- src/app/components/ServicesOffer.js
+++ src/app/components/ServicesOffer.js
@@ -4,37 +4,37 @@ const ServicesOffer = () => {
       <h1>We offer various services</h1>
       <div className="services-offer-slider">
         <div className="cards">
-          <div className="card" style={{ "--position": 1 }}>
+          <div className="card" style={{ "--position": 6 }}>
             <img src="/offer1.jpg" />
             <div className="overlay">
               <p>Motion Graphics</p>
             </div>
           </div>
-          <div className="card" style={{ "--position": 2 }}>
+          <div className="card" style={{ "--position": 5 }}>
             <img src="/offer2.jpg" />
             <div className="overlay">
               <p>2D & 3D Animation</p>
             </div>
           </div>
-          <div className="card" style={{ "--position": 3 }}>
+          <div className="card" style={{ "--position": 4 }}>
             <img src="/offer3.jpg" />
             <div className="overlay">
               <p>Social Media Content</p>
             </div>
           </div>
-          <div className="card" style={{ "--position": 4 }}>
+          <div className="card" style={{ "--position": 3 }}>
             <img src="/offer4.jpg" />
             <div className="overlay">
               <p>Video Editing</p>
             </div>
           </div>
-          <div className="card" style={{ "--position": 5 }}>
+          <div className="card" style={{ "--position": 2 }}>
             <img src="/offer5.jpg" />
             <div className="overlay">
               <p>VFX & Effects</p>
             </div>
           </div>
-          <div className="card" style={{ "--position": 6 }}>
+          <div className="card" style={{ "--position": 1 }}>
             <img src="/offer6.jpg" />
             <div className="overlay">
               <p>Graphic Design</p>
